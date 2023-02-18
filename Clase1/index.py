ae3f5b32b5206eaf35e7c4d4be7409efda0deecc
403 <403@academicos.cesde.edu.co>
2023-02-18 13:15:27 -0500

    Se crea programa que calculo los multiplos de 5

--- Clase1/index.py
+++
@@ -1,5 +0,0 @@
-numero1= int (input('Digite un numero:'))
-numero2=int (input('Digite un numero:'))
-
-print(f"La suma es {suma}")
-
---
+++ Clase1/multiplo.py
@@ -0,0 +1,7 @@
+numero1= int(input("igite el numero:"))
+if(numero1%5==0):
+    print(f"El numero {numero1} si es multiplo de 5")
+else:
+    print(f"El nuemro {numero1} no es multiplo de 5")
+
+
