6d598bcc4e8d13a7faafcd1f6616fdf0dce914d0
William Emir Mercado Salas <wmercado@cuerosvelez.com>
2023-02-22 10:08:02 -0500

    se crea proceso para encontrar los multiplos de 7

---
+++ condicionales/cicloMultiplos.py
@@ -0,0 +1,6 @@
+lista=int(input("Digite el tamaño de la lista:"))
+
+print("Los multiplos de 7 son")
+for multiplos in range(7, lista+1, 7):
+    print("[",multiplos,"],")
+    
--- condicionales/multiplo_Lista.py
+++
@@ -1,5 +0,0 @@
-lista=int(input("Digite el tamaño de la lista:"))
-
-
-for multiplos in lista, 7, 7:
-    print(f"Los multiplos de 7 son:{multiplos}")
