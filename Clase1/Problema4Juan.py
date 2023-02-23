42a5747b5ebca094be3da2a04f7ab0ff86aa9db4
William Emir Mercado Salas <wmercado@cuerosvelez.com>
2023-02-23 12:32:15 -0500

    Se modifico el problema4 de juan y camila

--- Clase1/Problema4Juan.py
+++ Clase1/Problema4Juan.py
@@ -4,5 +4,5 @@ camila=juan/2
 
 ricardo = camila + juan/2
 
-print(f"La cantidad de dinero de camila es {camila}")
-print(f"La cantidad de dinero de ricardo es {ricardo}")
+print(f"La cantidad de PLATA de camila es {camila}")
+print(f"La cantidad de PLATA de ricardo es {ricardo}")
